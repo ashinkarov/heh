41e15d4a4c67ce2fdc06655f3c979c4eda438d87
Artem Shinkarov <artyom.shinkaroff@gmail.com>
2017-05-11 01:04:45 +0100

    Parse programs from file.

--- main.ml
+++ main.ml
@@ -3,11 +3,39 @@ open Printf;;
 open Ast;;
 open Traverse;;
 
+(* XXX these variables are not in use and serve an example on how to handle
+   command line arguments.  *)
+let somestr = ref ""
+let someint = ref 0
 
-let load file =
-  let lexbuf = Lexing.from_channel file in
-  let e = Parser.prog Lexer.token lexbuf in
-  printf "%s" (expr_to_str (app_to_hof e))
-;;
+(* The name of the file we parse.  *)
+let fname = ref ""
 
-load stdin;;
+(* Helper string that will be printed at the begining of the help message
+   triggered by --help flag.  *)
+let usage = "usage: "
+
+
+(* XXX this is an example on how to handel command-line arguments.  *)
+let arglist = [
+    ("-s", Arg.String (fun s -> somestr := s), ": follows -s sets some string");
+    ("-d", Arg.Int    (fun d -> someint := d), ": some int parameter");
+  ]
+
+let main =
+    Arg.parse_argv
+        Sys.argv
+        arglist
+        (fun x -> if !fname <> "" then
+                    raise (ImapFailure "Multiple input files found on command line")
+                  else
+                    fname := x)
+        usage;
+   
+    let file = open_in !fname in
+    let lexbuf = Lexing.from_channel file in
+    let e = Parser.prog Lexer.token lexbuf in
+    printf "%s" (expr_to_str (app_to_hof e));
+    close_in file
+
+let () = main
